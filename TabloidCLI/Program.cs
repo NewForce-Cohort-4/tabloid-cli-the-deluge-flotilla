613ed0c49a7ba466275819c050b3c8cb713a0722
Detective-Coder <aaronfrankenfield@gmail.com>
2021-05-04 16:04:11 -0400

    just getting set up

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -1,4 +1,5 @@
-﻿using TabloidCLI.UserInterfaceManagers;
+﻿using System;
+using TabloidCLI.UserInterfaceManagers;
 
 namespace TabloidCLI
 {
@@ -13,6 +14,7 @@ namespace TabloidCLI
                 // Each call to Execute will return the next IUserInterfaceManager we should execute
                 // When it returns null, we should exit the program;
                 ui = ui.Execute();
+                Console.WriteLine("test");
             }
         }
     }
